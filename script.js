666829f71c580acf109ada9f088084225ada036b
NicolaPlacentino <nyko0921@gmail.com>
2022-12-02 14:58:15 +0100

    added DOM element variable

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21 */
+
+// Prendere l'elemento dal DOM
+// Chidere il nome all'utente e salvarlo
+// Chidere il cognome all'utente e salvarlo
+// Chidere il colore preferito all'utente e salvarlo
+// Stampare nome + cognome + colore preferito + 21 nel DOM
+
+const targetElement = document.getElementById('target')
